acc0ed71bc4e55aef60f528a7133917042cde68a
m05769js <jack.strange@student.manchester.ac.uk>
2022-02-18 11:08:12 +0000

    Implement ants with basic movement onscreen
    Ants are now generated and move randomly onscreen. They bounce off of
    walls.
    
    Ants are built with a Builder Pattern class, AntBuilder. This saves the
    effort of dealing with 'default' parameters in Java.

--- src/Environment.java
+++ src/Environment.java
@@ -1,31 +1,51 @@
-import javax.swing.*;
-
-import java.awt.event.ActionListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
-import java.awt.*; // VSCode won't pick up java.awt packages. I will fix this later.
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import ants.Ant;
 
 public class Environment extends JPanel implements ActionListener {
     private Timer t;
-    private int x = 0;
-    private int y = 0;
+    private int antCount = 400;
 
-    public Environment(){
+    private Random RANDOM = new Random();
+
+    private ArrayList<Ant> ants;
+
+    public Environment() {
+        ants = new ArrayList<Ant>();
+        for (int i = 0; i < antCount; i++) {
+            Ant newAnt = new Ant.AntBuilder().x(RANDOM.nextDouble() * 1000).
+                                              y(RANDOM.nextDouble() * 1000).
+                                              theta(RANDOM.nextDouble()*360.0).
+                                              buildAnt();
+            ants.add(newAnt);
+        }
         t = new Timer(1000/30, this);
         t.start();
     }
 
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2D = (Graphics2D) g;
 
-        g2D.fillOval(x, y, 100, 100);
+        for (Ant ant : ants) {
+            g2D.fillOval((int) ant.x-ant.width/2, (int) ant.y-ant.width/2, ant.width, ant.width);
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        x+=20;
-        y+=20;
+        for(Ant ant:ants){
+            ant.move();
+        }
         repaint();
     }
-    
+
 }
---
+++ src/ants/Ant.java
@@ -0,0 +1,114 @@
+package ants;
+
+import java.util.Random;
+
+public class Ant {
+    public double x, y, theta, velocity, visionRange, visionRadius;
+    public int width;
+    private static Random RANDOM = new Random(); 
+
+    private Ant(double x, double y, double theta, double velocity, double visionRange, double visionRadius, int width) {
+        this.x = x;
+        this.y = y;
+        this.theta = theta;
+        this.velocity = velocity;
+        this.visionRange = visionRange;
+        this.visionRadius = visionRadius;
+        this.width = width;
+    }
+
+    public void move(){
+        double xchange = Math.cos(Math.PI / 180.0 * theta) * velocity;
+        double ychange = -Math.sin(Math.PI / 180.0 * theta) * velocity;
+
+        // Bounce if extending horizontally
+        if (x+xchange > 1000){
+            x = 1000.0;
+            theta = 180.0 - theta;
+        }else if(x+xchange < 0) {
+            x = 0.0;
+            theta = 180.0 - theta;
+        }else{
+            x += xchange;
+        }
+
+        // Bounce if extending vertically
+        if (y+ychange > 1000){
+            y = 1000.0;
+            theta = -theta;
+        }else if(y+ychange < 0) {
+            y = 0.0;
+            theta = -theta;
+        }else{
+            y += ychange;
+        }
+        
+        // Adjust theta within range
+        while(theta < 0){
+            theta += 360;
+        }
+        while(theta > 360){
+            theta -= 360;
+        }
+        theta += ((RANDOM.nextDouble()-0.5) * 2.0)*15.0;
+    }
+
+    // This class makes "default" values easier to handle. Uses the Builder Pattern
+    public static class AntBuilder {
+        private double x = 0;
+        private double y = 0;
+        private double theta = 0;
+        private double velocity = 10;
+        private double visionRange = 40;
+        private double visionRadius = 180;
+        public int width = 10;
+        
+        public AntBuilder(){
+            x = 0;
+            y = 0;
+            theta = 0;
+            velocity = 10;
+            visionRange = 40;
+            visionRadius = 180;
+        }
+
+        public Ant buildAnt() {
+            return new Ant(x, y, theta, velocity, visionRange, visionRadius, width);
+        }
+
+        public AntBuilder x(double x) {
+            this.x = x;
+            return this;
+        }
+
+        public AntBuilder y(double y) {
+            this.y = y;
+            return this;
+        }
+
+        public AntBuilder theta(double theta) {
+            this.theta = theta;
+            return this;
+        }
+
+        public AntBuilder velocity(double velocity) {
+            this.velocity = velocity;
+            return this;
+        }
+
+        public AntBuilder visionRange(double visionRange) {
+            this.visionRange = visionRange;
+            return this;
+        }
+
+        public AntBuilder visionRadius(double visionRadius) {
+            this.visionRadius = visionRadius;
+            return this;
+        }
+
+        public AntBuilder wdith(int width) {
+            this.width = width;
+            return this;
+        }
+    }
+}
