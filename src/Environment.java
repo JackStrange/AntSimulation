6439ad39547289e2b0e38418948fea2622bcb0b8
m05769js <jack.strange@student.manchester.ac.uk>
2022-02-18 11:47:51 +0000

    Develop Food class and food creation system
    This new code creates a new class - food - which the ants will attempt
    to take back to their colony. Food is generated whenever the user
    clicks on the screen. Food is generated in a cluster, with a distance
    from the mouse click position generated under a Gaussian (normal)
    distribution.

--- src/Environment.java
+++ src/Environment.java
@@ -3,22 +3,26 @@ import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import javax.swing.event.MouseInputAdapter;
 
 import ants.*;
 
 public class Environment extends JPanel implements ActionListener {
     private Timer t;
     private int antCount = 400;
+    private int foodCountPC = 500; // Food per food cluster
 
     private Random RANDOM = new Random();
 
     private ArrayList<Ant> ants = new ArrayList<Ant>();
     private ArrayList<Colony> colonies = new ArrayList<Colony>();
+    private ArrayList<Food> foods = new ArrayList<Food>();
 
     public Environment() {
         colonies.add(new Colony(500.0,500.0,100,0x0066FF));
@@ -28,6 +32,30 @@ public class Environment extends JPanel implements ActionListener {
                                               buildAnt();
             ants.add(newAnt);
         }
+        this.addMouseListener(new MouseInputAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // This code forms "clusters" that food land in
+                // It spawns a cluster at wherever the user clicks
+                // It then generates food objects in the vicinity (normally distributed)
+                double x = e.getX();
+                double y = e.getY();
+                for (int j = 0; j < foodCountPC; j++){
+                    // Generates a normally distributed radius. Capped at 50 pixels away
+                    double radius = 0;
+                    do{
+                        radius = Math.abs(RANDOM.nextGaussian());
+                    }while(radius > 2.0);
+                    radius *= 25.0;
+                    // Generate a random angle, and adjust x and y values
+                    double theta = RANDOM.nextDouble()*360.0;
+                    double deltax = Math.cos(Math.PI/180.0*theta)*radius;
+                    double deltay = -Math.sin(Math.PI/180.0*theta)*radius;
+                    foods.add(new Food(x+deltax, y+deltay, 10));
+                }
+            }
+        });
+
         t = new Timer(1000/30, this);
         t.start();
     }
@@ -42,6 +70,10 @@ public class Environment extends JPanel implements ActionListener {
         for (Colony colony : colonies) {
             g2D.setColor(new Color(colony.colour));
             g2D.fillOval((int) colony.x-colony.width/2, (int) colony.y-colony.width/2, colony.width, colony.width);
+        }
+        g2D.setColor(Color.GREEN);
+        for (Food food : foods) {
+            g2D.fillOval((int) food.x-food.width/2, (int) food.y-food.width/2, food.width, food.width);
         }
     }
 
---
+++ src/ants/Food.java
@@ -0,0 +1,12 @@
+package ants;
+
+public class Food {
+    public double x, y;
+    public int width;
+    
+    public Food(double x, double y, int width) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+    }
+}
